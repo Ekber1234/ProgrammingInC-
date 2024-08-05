b376f4f38bd5a23d4e104baf2f1e28aa50116508
Ekber1234 <ekber1906@gmail.com>
2024-08-05 09:39:00 +0400

    Project initially added

---
+++ Ders1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ders1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number");
+
+            int var1;
+            while (true)
+            {
+                try
+                {
+                    var1 = int.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (Exception exc)
+                {
+                    Console.WriteLine("You can only enter int number");
+                }
+            }
+
+            Console.WriteLine("Enter second number");
+
+            int var2;
+            while (true)
+            {
+                try
+                {
+                    var2 = int.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (Exception exc)
+                {
+                    Console.WriteLine("You can only enter int number");
+                }
+            }
+
+            int sum = Sum(var1, var2);
+            Console.WriteLine("Sum is: " + sum);
+            Console.ReadLine();
+        }
+        public static int Sum(int var1, int var2)
+        {
+            return var1 + var2;
+        }
+    }
+}
